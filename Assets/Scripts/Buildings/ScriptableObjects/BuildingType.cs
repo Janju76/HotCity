9c86adec3d1ccd4dbea3a133bc4027ac40f3379f
DESKTOP-OJPSGME\erik <erik@bitpunked.com>
2018-09-08 12:28:34 +0200

    1st commit! ERSTER! :D + added BuildingType

---
+++ Assets/Scripts/Buildings/ScriptableObjects/BuildingType.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newBuildingType", menuName = "HotCity/New Building Type")]
+public class BuildingType : ScriptableObject {
+}
