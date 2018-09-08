cec941f76ca3857735d7b808722f5113721cf12d
Florian Munz <>
2018-09-08 14:39:54 +0200

    Camera looks at Target. Target can be moved with keys WASD and rotated with QR. Script "CameraTarget"is to add to the grid.

---
+++ Assets/FloxTemp/CameraTarget.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour 
+{
+    public GameObject _Camera;
+    public GameObject _target;
+
+    public float camDistance = 100f;
+
+    public float rotateSpeed = 30f;
+    public float translateSpeed = 30f;
+
+    void Start(){
+
+        _Camera.transform.position = new Vector3(0, camDistance, -camDistance);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+           _target.transform.Translate(Vector3.forward * translateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _target.transform.Translate(Vector3.forward * -translateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _target.transform.Translate(Vector3.right * -translateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _target.transform.Translate(Vector3.right * translateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _target.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            _target.transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+    }
+
+
+}
