bca51bd05bbd46e9c7eb510ce62a96335af546f6
DESKTOP-OJPSGME\erik <erik@bitpunked.com>
2018-09-08 14:54:29 +0200

    BuildingTypes Relations

--- Assets/Scripts/Buildings/ScriptableObjects/BuildingType.cs
+++ Assets/Scripts/Buildings/ScriptableObjects/BuildingType.cs
@@ -2,7 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EBuildingTypeRelation
+{
+    NEUTRAL,
+    GOOD,
+    BAD
+}
+
 [CreateAssetMenu(fileName = "newBuildingType", menuName = "HotCity/Buildings/New Building Type")]
 public class BuildingType : ScriptableObject {
-     
+
+    public List<BuildingType> goodNeighbour = new List<BuildingType>();
+    public List<BuildingType> badNeighbour = new List<BuildingType>();
+
+
+    /// <summary>
+    /// Returns the BuildingType Relation
+    /// </summary>
+    /// <param name="otherType"></param>
+    /// <returns></returns>
+    public EBuildingTypeRelation GetBuildingTypeRelation(BuildingType otherType)
+    {
+        if (goodNeighbour.Contains(otherType))
+            return EBuildingTypeRelation.GOOD;
+        else
+        if (badNeighbour.Contains(otherType))
+            return EBuildingTypeRelation.BAD;
+        else
+            return EBuildingTypeRelation.NEUTRAL;
+
+    }
 }
