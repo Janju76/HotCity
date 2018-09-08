5546bb4d4d69cb570987c3aa6ba09121ba3bdd68
Florian Munz <>
2018-09-08 15:54:27 +0200

    Camera Bounds, SetCamDistance function

--- Assets/FloxTemp/CameraTarget.cs
+++ Assets/FloxTemp/CameraTarget.cs
@@ -12,6 +12,8 @@ public class CameraTarget : MonoBehaviour
     public float rotateSpeed = 30f;
     public float translateSpeed = 30f;
 
+    public float bounds = 150f;
+
     void Start(){
 
         _Camera.transform.position = new Vector3(0, camDistance, -camDistance);
@@ -23,21 +25,26 @@ public class CameraTarget : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
            _target.transform.Translate(Vector3.forward * translateSpeed * Time.deltaTime);
+
+            CheckBounds(_target.transform.position);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             _target.transform.Translate(Vector3.forward * -translateSpeed * Time.deltaTime);
+            CheckBounds(_target.transform.position);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             _target.transform.Translate(Vector3.right * -translateSpeed * Time.deltaTime);
+            CheckBounds(_target.transform.position);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             _target.transform.Translate(Vector3.right * translateSpeed * Time.deltaTime);
+            CheckBounds(_target.transform.position);
         }
 
         if (Input.GetKey(KeyCode.Q))
@@ -51,5 +58,37 @@ public class CameraTarget : MonoBehaviour
         }
     }
 
+    public void SetCamDistance(float distance)
+    {
+        _Camera.transform.position = new Vector3(0, distance, -distance);
+    }
 
+    public void CheckBounds(Vector3 targetPosition)
+    {
+        Vector3 nextPosi = targetPosition;
+
+        bool xOutOfBounds = false;
+        bool zOutOfBounds = false;
+        if (nextPosi.x > bounds || (nextPosi.x < -bounds)) xOutOfBounds = true;
+        if (nextPosi.y > bounds || (nextPosi.z < -bounds)) zOutOfBounds = true;
+
+        if(xOutOfBounds && !zOutOfBounds){
+            if (nextPosi.x > 0) _target.transform.position = new Vector3(bounds, 0, nextPosi.z);
+            else _target.transform.position = new Vector3(-bounds, 0, nextPosi.z);
+        }
+
+        if (!xOutOfBounds && zOutOfBounds)
+        {
+            if (nextPosi.z > 0) _target.transform.position = new Vector3(nextPosi.x, 0, bounds);
+            else _target.transform.position = new Vector3(nextPosi.x, 0, -bounds);
+        }
+
+        if (xOutOfBounds && zOutOfBounds)
+        {
+            if (nextPosi.x > 0 && nextPosi.z > 0) _target.transform.position = new Vector3(bounds, 0, bounds);
+            else if (nextPosi.x > 0 && nextPosi.z < 0) _target.transform.position = new Vector3(bounds, 0, -bounds);
+            else if (nextPosi.x < 0 && nextPosi.z > 0) _target.transform.position = new Vector3(-bounds, 0, bounds);
+            else if (nextPosi.x < 0 && nextPosi.z < 0) _target.transform.position = new Vector3(-bounds, 0, -bounds);
+        }
+    }
 }
